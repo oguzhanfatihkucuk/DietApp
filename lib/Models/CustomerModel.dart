c31ae40065accb4ae24a5a75e0824a1ff38aad49
Oğuzhan Fatih Küçük <83452551+phoenix9897@users.noreply.github.com>
2025-02-09 16:28:43 +0300

    Models have been updated.

--- lib/Models/CustomerModel.dart
+++ lib/Models/CustomerModel.dart
@@ -1,35 +1,111 @@
+import 'WaterConsumption.dart';
 import 'DietPlanModel.dart';
+import 'DietaryHabits.dart';
+import 'Goals.dart';
+import 'HealthStatus.dart';
 
 class Customer {
-  final String firstName;
-  final String lastName;
-  final String email;
-  final String phone;
-  final int age;
-  final String gender;
-  final double height;
-  final double weight;
-  final String healthStatus;
-  final String dietaryHabits;
-  final double dailyWaterAmount;
-  final String waterConsumptionHabit;
-  final String goals;
-  final List<DietPlan> dietPlans;
+  int customerID;
+  int dietitianID;
+  String firstName;
+  String lastName;
+  String email;
+  String phone;
+  bool isLoginBefore;
+  int age;
+  String gender;
+  double height;
+  double weight;
+  double bodyMassIndex;
+  double targetWeight;
+  String activityLevel;
+
+  HealthStatus healthStatus;
+  DietaryHabits dietaryHabits;
+  WaterConsumption waterConsumption;
+  Goals goals;
+
+  List<DietPlan> dietPlans;
+  List<dynamic> progressTracking;
+  List<dynamic> weeklyMeals;
 
   Customer({
+    required this.customerID,
+    required this.dietitianID,
     required this.firstName,
     required this.lastName,
     required this.email,
     required this.phone,
+    required this.isLoginBefore,
     required this.age,
     required this.gender,
     required this.height,
     required this.weight,
+    required this.bodyMassIndex,
+    required this.targetWeight,
+    required this.activityLevel,
     required this.healthStatus,
     required this.dietaryHabits,
-    required this.dailyWaterAmount,
-    required this.waterConsumptionHabit,
     required this.goals,
+    required this.waterConsumption,
     required this.dietPlans,
+    required this.progressTracking,
+    required this.weeklyMeals,
+    
   });
+
+  factory Customer.fromJson(Map<String, dynamic> json) {
+    return Customer(
+      customerID: json['customerID'],
+      dietitianID: json['dietitianID'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      email: json['email'],
+      phone: json['phone'],
+      isLoginBefore: json['isLoginBefore'] ?? false,
+      age: json['age'],
+      gender: json['gender'],
+      height: json['height'].toDouble(),
+      weight: json['weight'].toDouble(),
+      bodyMassIndex: json['bodyMassIndex']?.toDouble(),
+      targetWeight: json['targetWeight']?.toDouble(),
+      activityLevel: json['activityLevel'],
+      healthStatus: HealthStatus.fromJson(json['healthStatus']),
+      dietaryHabits: DietaryHabits.fromJson(json['dietaryHabits']),
+      waterConsumption: WaterConsumption.fromJson(json['waterConsumption']),
+      goals: Goals.fromJson(json['goals']),
+      dietPlans: (json['dietPlans'] as List)
+          .map((plan) => DietPlan.fromJson(plan))
+          .toList(),
+      progressTracking: json['progressTracking'] ?? [],
+      weeklyMeals: json['weeklyMeals'] ?? [],
+
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'customerID': customerID,
+      'dietitianID': dietitianID,
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'phone': phone,
+      'isLoginBefore': isLoginBefore,
+      'age': age,
+      'gender': gender,
+      'height': height,
+      'weight': weight,
+      'bodyMassIndex': bodyMassIndex,
+      'targetWeight': targetWeight,
+      'activityLevel': activityLevel,
+      'healthStatus': healthStatus.toJson(),
+      'dietaryHabits': dietaryHabits.toJson(),
+      'waterConsumption': waterConsumption.toJson(),
+      'goals': goals.toJson(),
+      'dietPlans': dietPlans.map((plan) => plan.toJson()).toList(),
+      'progressTracking': progressTracking,
+      'weeklyMeals': weeklyMeals,
+    };
+  }
 }
--- lib/Models/DietPlanModel.dart
+++ lib/Models/DietPlanModel.dart
@@ -1,4 +1,5 @@
-import 'MealModel.dart';
+import 'package:intl/intl.dart';
+import 'Meal.dart';
 
 class DietPlan {
   String planID;
@@ -22,4 +23,34 @@ class DietPlan {
     required this.dailyCarbohydrateTarget,
     required this.meals,
   });
+
+  factory DietPlan.fromJson(Map<String, dynamic> json) {
+    return DietPlan(
+      planID: json['planID'],
+      planName: json['planName'],
+      startDate: json['startDate'],
+      endDate: json['endDate'],
+      dailyCalorieTarget: json['dailyCalorieTarget'],
+      dailyProteinTarget: json['dailyProteinTarget'],
+      dailyFatTarget: json['dailyFatTarget'],
+      dailyCarbohydrateTarget: json['dailyCarbohydrateTarget'],
+      meals: (json['meals'] as List)
+          .map((meal) => Meal.fromJson(meal))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'planID': planID,
+      'planName': planName,
+      'startDate': DateFormat('yyyy-MM-dd').format(startDate), // DateTime → String
+      'endDate': DateFormat('yyyy-MM-dd').format(endDate), // DateTime → String
+      'dailyCalorieTarget': dailyCalorieTarget,
+      'dailyProteinTarget': dailyProteinTarget,
+      'dailyFatTarget': dailyFatTarget,
+      'dailyCarbohydrateTarget': dailyCarbohydrateTarget,
+      'meals': meals.map((meal) => meal.toJson()).toList(),
+    };
+  }
 }
